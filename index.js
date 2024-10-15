74049355520ec167f452da195cc21d1fb9328349
mdani1201 M <modani1201@gmail.com>
2024-10-15 11:26:28 +0000

    connect to mongo + created Schemas for user, exercise, logs + auto restart by save

--- index.js
+++ index.js
@@ -1,18 +1,77 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const { v4: uuidv4 } = require('uuid');
+const bodyParser = require("body-parser");
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {res.sendFile(__dirname + "/views/index.html");});
+app.use("/", bodyParser.urlencoded({ extended: false }));
 
+const ExerciseSchema = new mongoose.Schema({
+  username: String,
+  description: String,
+  duration: Number,
+  date: Date,
+  _id: String,
+});
 
+const UserSchema = new mongoose.Schema({
+  username: String,
+  _id: String,
+});
 
+const LogSchema = new mongoose.Schema({
+  username: String,
+  count: Number,
+  _id: String,
+  log: [
+    {
+      description: String,
+      duration: Number,
+      date: Date,
+    },
+  ],
+});
+
+const User = mongoose.model("User", UserSchema);
+const Log = mongoose.model("User", LogSchema);
+const Exercise = mongoose.model("User", ExerciseSchema);
+
+const createUserObject = (username, done) => {
+  Url.create(
+    {
+      username: username,
+      _id: uuidv4()
+    },
+    (err, data) => {
+      if (err) {
+        return done(err);
+      } else {
+        done(null, data);
+      }
+    }
+  );
+};
+
+app.post("/api/users", (req, res) => {
+  createUserObject(req., (err, data) => {
+    if (err) return res.json({ error: err });
+    res.json({
+      username: data.username,
+      _id: data._id
+    });
+  });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
